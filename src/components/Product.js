dbed6b5c96d57588d37bd2cce90b6f1a119696de
Erik Nakata <erik.nakata5@gmail.com>
2017-12-16 20:56:43 -0200

    New components

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,22 +1,20 @@
 import React, { Component } from 'react';
 import '../assets/css/product.css'
-import bud from '../assets/images/bud.jpg'
 
 export default class Product extends Component {
-    
+
     render() {
         return (
-            <div className="product-cart">
-                <img src={bud} width="150" height="150" alt={this.props.productName} title={this.props.productName} />
-                <div className="product-title">
-                    <span>{this.props.productName}</span>
-                </div>
-                <div className="product-price">
-                    <span>R$ {this.props.price}</span>
-                </div>
+            <div className="product-card">
+                <img src={this.props.imageUrl} width="150" height="150" alt={this.props.productName} title={this.props.productName} />
+                <span className="product-title">{this.props.productName}</span>
+                <span className="product-price">R$ {this.props.price}</span>
                 <div className="product-buttons">
                     <div className="btn-subtract">
                         -
+                    </div>
+                    <div className="box-quantity">
+                        <span>{this.props.quantity}</span>
                     </div>
                     <div className="btn-plus">
                         +
--- src/components/Products.js
+++ src/components/Products.js
@@ -7,12 +7,10 @@ export default class Products extends Component {
     render() {
         return (
             <div className="products-container">
-                <Product productName="Corona Extra 355ml - Pack com 6 unidades" price="3.49" />
-                <Product productName="Corona Extra 355ml - Pack com 6 unidades fewfw efw" price="3.49" />
-                <Product productName="Corona Extra 355ml" price="3.49" />
-                <Product productName="Corona Extra 355ml - Pack com 6 unidades" price="3.49" />
-                <Product productName="Corona Extra 355ml - Pack com 6 unidades" price="3.49" />
-                <Product productName="Corona Extra 355ml - Pack com 6 unidades fewfw efw" price="3.49" />
+                <Product productName="Budweiser 350ml - Unidade" price="3.49" quantity="0" imageUrl="https://s3-us-west-2.amazonaws.com/ze-delivery/upload/images/5727e7aaa2c99.jpg" />
+                <Product productName="Skol 350ml - Unidade" price="2.99" quantity="0" imageUrl="https://s3-us-west-2.amazonaws.com/ze-delivery/upload/images/5662f9a0bfd2e.jpg" />
+                <Product productName="Stella Artois 275ml - Pack com 6 unidades" price="23.94" quantity="0" imageUrl="https://s3-us-west-2.amazonaws.com/ze-delivery/upload/images/590cc86383b63.jpg" />
+                <Product productName="Stella Artois 275ml - Unidade" price="3.99" quantity="0" imageUrl="https://s3-us-west-2.amazonaws.com/ze-delivery/upload/images/592de3ed8977d.jpg" />
             </div>
         );
     }
